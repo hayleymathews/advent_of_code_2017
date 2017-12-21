f1606b8cceac44e07abd2f8d21b102c080a9d4c9
Hayley Mathews <hayley.mathews@aviso.com>
2017-12-21 09:33:31 -0800

    day 2 in clojure, plus readme

--- src/day_01/solution_01.clj
+++ src/day_01/solution_01.clj
@@ -11,9 +11,7 @@
 
 (defn captcha-2 [numbers]
   (let [split-numbers   (split-at (/ (count numbers) 2) numbers)
-        front-half      (first split-numbers)
-        back-half       (last split-numbers)
-        mixed-numbers   (interleave front-half back-half)]
+        mixed-numbers   (interleave (first split-numbers)  (last split-numbers)]
     (->> mixed-numbers
          (partition 2)
          (filter #(= (first %) (second %)))
--- src/day_02/solution_02.clj
+++ src/day_02/solution_02.clj
@@ -1,1 +1,9 @@
-;; solve me :(
+;;
+
+
+(defn checksum-1 [matrix]
+  (->> matrix
+       (map (fn [row] (- (apply max row) (apply min row))))
+       (reduce +)
+  )
+)
